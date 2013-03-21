583de4b9df83065ca7fe5c8f7e56491ca43992de
eldwin <eldwin_@hotmail.com>
2013-03-21 22:27:26 +0800

    removed unnecessary code from organization model

--- app/models/open_badges/organization.rb
+++ app/models/open_badges/organization.rb
@@ -4,26 +4,11 @@ module OpenBadges
 
     validates :url, :name, presence: true
 
-    after_initialize :assign_defaults
-
-
-    private
-    def assign_defaults
-      if new_record?
-        self.url ||= ""
-        self.name ||= ""
-        self.image ||= ""
-        self.email ||= ""
-        self.description ||= ""
-      end
-    end
-
     public
     def as_json(options = nil)
       super(
-      	:only => [:url, :name, :image, :email, :description]
-      ).reject{ |key, value| value.nil? || value.empty? }
+        :only => [:url, :name, :image, :email, :description]
+      ).reject{ |key, value| value.empty? }
     end
-
   end
 end
