3d76aae886285a0f27f560f7e06e3e2da3335b15
Andrew Eng <andrew.engwy@gmail.com>
2013-04-13 17:14:39 +0800

    Fixed organization controller bug

--- lib/open_badges/attachment_helper.rb
+++ lib/open_badges/attachment_helper.rb
@@ -2,12 +2,6 @@ module AttachmentHelper
 
   ATTACHMENT_URL = ":mount_path:class_without_namespace/:attachment/:id.:extension"
   MISSING_IMAGE_URL = "open_badges/missing.png"
-
-  def image_url
-    Rails.application.routes.url_helpers.root_url({
-      :host => Rails.application.config.default_url_options[:host]
-    }).gsub(/\/$/, '') + image.url(:original, false)
-  end
   
   Paperclip.interpolates :mount_path do |attachment, style|
     OpenBadges::Engine.routes.url_helpers.root_path
